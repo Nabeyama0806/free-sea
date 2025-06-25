ff060f18439df2ac3f8f6f27fa3ad4c0fae86d5d
hmj2422021 <hmj2422021@stu.o-hara.ac.jp>
2025-06-25 21:07:44 +0900

    ステージの変更

--- Stage.h
+++ Stage.h
@@ -6,17 +6,18 @@ class Stage : public ModelActor
 private:
 	static constexpr int FrameIndex = -1;
 
-	ModelActor* m_mesh;
+	ModelActor* m_model;
 	int m_modelHandle;
 
 public:
 	//コンストラクタ
 	Stage() :
-		ModelActor("StageMesh", "Environment/Ground_Mesh.mv1"),
+		ModelActor("StageMesh", "Environment/Panel_Mesh.mv1"),
+		m_model(nullptr),
 		m_modelHandle(0)
 	{
-		m_mesh = new ModelActor("Stage", "Environment/Ground.mv1");
-		AddChild(m_mesh);
+		//見た目のモデルを読み込む
+		AddChild(new ModelActor("Stage", "Environment/Panel.mv1"));
 	}
 
 	int GetFrameIndex()
