1c837a536d73affe736a318088d00039cc4cd852
hmj2422021 <hmj2422021@stu.o-hara.ac.jp>
2025-06-29 23:13:07 +0900

    勝利したプレイヤーの画像を表示する準備

--- SceneResult.cpp
+++ SceneResult.cpp
@@ -2,9 +2,10 @@
 #include "SceneTitle.h"
 #include "SceneManager.h"
 #include "SoundManager.h"
+#include "Sprite.h"
 #include "SpriteActor.h"
+#include "SpriteAnimation.h"
 #include "Screen.h"
-#include "Input.h"
 #include "Time.h"
 
 //初期化
@@ -17,6 +18,15 @@ void SceneResult::Initialize()
 
 	//効果音の再生
 	SoundManager::Play("Resource/Sound/se_winner.mp3");
+
+	//姿勢情報
+	m_transform.position = Screen::Center;
+
+	//画像の読み込み
+	m_sprite = new Sprite();
+	m_sprite->Register(TextureName[m_playerIndex], SpriteAnimation(TextureFilePath[m_playerIndex]));
+	m_sprite->Load();
+	m_sprite->Play(TextureName[m_playerIndex]);
 }
 
 //終了処理
@@ -33,13 +43,16 @@ void SceneResult::Update()
 	//ノードの更新
 	m_rootNode->TreeUpdate();
 
+	//画像の更新
+	m_sprite->Update();
+
 	if (m_waitTransitionTime > 0)
 	{
 		m_waitTransitionTime -= Time::GetInstance()->GetDeltaTime();
 	}
 
 	//いずれかのキーが押されたらタイトル画面に遷移
-	if (m_waitTransitionTime <= 0 && Input::GetInstance()->IsAnyKeyDown())
+	if (m_waitTransitionTime <= 0 && InputSystem::GetInstance()->IsAnyKeyDown())
 	{
 		SceneManager::GetInstance()->LoadScene(new SceneTitle());
 	}
@@ -50,4 +63,7 @@ void SceneResult::Draw()
 {
 	//ノードの更新
 	m_rootNode->TreeDraw();
+
+	//画像の表示
+	m_sprite->Draw(m_transform);
 }
--- SceneResult.h
+++ SceneResult.h
@@ -1,24 +1,47 @@
 #pragma once
 #include "SceneBase.h"
+#include "Transform.h"
+#include "InputSystem.h"
 
 class Node;
-class Character;
+class Sprite;
 
 class SceneResult : public SceneBase
 {
 private:
 	static constexpr float WaitTransitionTime = 1.5f;
 
-	Node* m_rootNode;
-	Character* m_character;
+	//画像のファイルパス
+	const char* TextureFilePath[InputSystem::MaxPadAmount] =
+	{
+		"Resource/Texture/title_logo.png",
+		"Resource/Texture/result_green.png",
+		"Resource/Texture/result_red.png",
+		"Resource/Texture/result_pink.png",
+	};
 
+	//画像の名前
+	const char* TextureName[InputSystem::MaxPadAmount] =
+	{
+		"blue_bird",
+		"green_bird",
+		"red_bird",
+		"pink_bird",
+	};
+
+	Node* m_rootNode;
+	Sprite* m_sprite;
+	Transform m_transform;
+
+	int m_playerIndex;
 	float m_waitTransitionTime;
 
 public:
 	//コンストラクタ
-	SceneResult(Character* character) :
-		m_character(character),
+	SceneResult(int playerIndex) :
+		m_playerIndex(playerIndex),
 		m_rootNode(nullptr),
+		m_sprite(nullptr),
 		m_waitTransitionTime(WaitTransitionTime) 
 	{
 	}
